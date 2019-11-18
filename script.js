9ec4b5d0da8279118a4d262cca44c4a8fe2011d0
Maureen Njeri <njerimaureen182@gmail.com>
2019-11-18 16:25:10 +0300

    first commit

---
+++ script.js
@@ -0,0 +1,9 @@
+// $(document).ready(function () {
+	
+// 	$('nav a[href*="#"]').on('click', function () {
+// 		// body...
+// 		$('html,body').animate(keyframes = {
+// 			scrollTop: $($(this).attr('href')).offset().top -100
+// 		}, options = 2000);
+// 	});
+// });
